1f52c332f7d96ac2bd129654b5de2a390062c686
DaniloLima122 <limadanilo06@gmail.com>
2020-08-01 01:08:34 -0300

    Finalizado parte de componentes

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react';
+
+import { GlobalContext } from "../context/GlobalState";
 
 export default function Balance() {
+
+    const { transactions } = useContext(GlobalContext);
+
+    const amounts = transactions.map(({ amount }) => amount);
+
+    const total = amounts.reduce((acc, current) => (acc + current), 0).toFixed(2);
+
     return (
         <>
             <h4>Your Balance</h4>
-            <h1 id="balance">$0.00</h1>
+            <h1 id="balance">R$ {total}</h1>
         </>
     )
 }
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react';
+
+import { GlobalContext } from "../context/GlobalState";
 
 export default function Expenses() {
+
+    const { transactions } = useContext(GlobalContext);
+
+    const receita = transactions
+        .filter(({ amount }) => amount > 0)
+        .reduce((acc, { amount }) => (acc + amount), 0).toFixed(2);
+
+
+    const despesa = (transactions
+        .filter(({ amount }) => amount < 0)
+        .reduce((acc, { amount }) => (acc + amount), 0) * -1).toFixed(2);
+
+    console.log(receita, despesa)
+
     return (
         <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
-                <p id="money-plus" className="money plus">+$0.00</p>
+                <p id="money-plus" className="money plus">R${receita.replace(".",",")}</p>
             </div>
             <div>
                 <h4>Expense</h4>
-                <p id="money-minus" className="money minus">-$0.00</p>
+                <p id="money-minus" className="money minus">R${despesa.replace(".",",")}</p>
             </div>
         </div>
     )
