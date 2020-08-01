88ceee0d62440363743230e1367dbac75bd43bcf
DaniloLima122 <limadanilo06@gmail.com>
2020-08-01 01:23:49 -0300

    Finalizado componente que adiciona novas transações

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,25 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react';
+
+import { GlobalContext } from "../context/GlobalState";
 
 export default function AddTransaction() {
 
     const [text, setText] = useState("");
     const [amount, setAmount] = useState(0);
 
+    const { addTransaction, transactions } = useContext(GlobalContext);
 
-    const InserirTexto = e => { 
+    const InserirTexto = e => {
 
         setText(e.target.value);
     }
 
-    const InserirQuantidade = e => { 
+    const InserirQuantidade = e => {
 
         setAmount(e.target.value);
+    }
+
+    const onSubmit = e => {
+
+        e.preventDefault();
+
+        const newTransaction = {
+
+            id: transactions.length + 1,
+            text,
+            amount: +amount
+        }
+
+        addTransaction(newTransaction);
+
     }
 
     return (
         <>
             <h3>Add new transaction</h3>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
                     <input type="text" id="text" value={text} onChange={InserirTexto} placeholder="Enter text..." />
@@ -29,7 +47,7 @@ export default function AddTransaction() {
                     >Amount <br />
             (negative - expense, positive - income)</label
                     >
-                    <input type="number" id="amount" value={amount} onChange={InserirQuantidade}  placeholder="Enter amount..." />
+                    <input type="number" id="amount" value={amount} onChange={InserirQuantidade} placeholder="Enter amount..." />
                 </div>
                 <button className="btn">Add transaction</button>
             </form>
