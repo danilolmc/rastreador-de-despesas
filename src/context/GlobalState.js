b8ef769957b7059f59e1eb8a6110e74172107211
DaniloLima122 <limadanilo06@gmail.com>
2020-08-01 01:25:12 -0300

    Finalizado componente que gerencia o estado global

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -6,13 +6,7 @@ import AppReducer from "./AppReducer";
 //Inicial State
 const inicialState = {
 
-    transactions: [
-
-        { id: 1, text: 'Flower', amount: -20 },
-        { id: 2, text: 'Salary', amount: 300 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 }
-    ]
+    transactions: []
 };
 
 
@@ -25,9 +19,29 @@ export const GlobalProvider = ({ children }) => {
 
     const [state, dispatch] = useReducer(AppReducer, inicialState);
 
+    //Actions
+    function deleteTransaction(id){
+
+        dispatch({
+            type: "DELETE_TANSACTION",
+            payload: id
+        });
+    }
+
+    function addTransaction(transaction){
+
+        dispatch({
+            type: "ADD_TANSACTION",
+            payload: transaction
+        });
+    }
+
+
     return (<GlobalContext.Provider value={{
-            transactions: state.transactions
-        }}>
+            transactions: state.transactions,
+            deleteTransaction,
+            addTransaction
+        }}> 
             {children}
         </GlobalContext.Provider>
     )
