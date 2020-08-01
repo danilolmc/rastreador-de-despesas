bedaef4e009213b6f3e6b08ae74969368269c753
DaniloLima122 <limadanilo06@gmail.com>
2020-08-01 01:24:55 -0300

    Finalizado componente reducer

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,7 +1,18 @@
-export default function name(state,action) {
+export default function (state, action) {
 
     switch (action.type) {
-        
+
+        case "DELETE_TANSACTION":
+            return {
+                ...state,
+                transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+            }
+
+        case "ADD_TANSACTION":
+            return{
+                ...state,
+                transactions: [...state.transactions,action.payload]
+            }
         default:
             return state;
     }
