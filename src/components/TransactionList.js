ddcc6652cca8094bc84ee61e0f1e89c1c7444d7d
DaniloLima122 <limadanilo06@gmail.com>
2020-08-01 01:24:21 -0300

    Finalizado componente que lista os items das transações

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react';
+
+import { GlobalContext } from "../context/GlobalState";
+
+import Transaction from './Transaction';
 
 export default function TransactionList() {
+
+    const { transactions } = useContext(GlobalContext);
+
     return (
         <>
             <h3>History</h3>
             <ul id="list" className="list">
-                <li className="minus">
-                    Cash <span>-$400</span><button className="delete-btn">x</button>
-                </li>
+                {
+                    transactions.map(transaction => (
+
+                        <Transaction key={transaction.id} transactions={transaction} />
+
+                    ))
+                }
+
             </ul>
         </>
     )
